4142297a70ece93bc5dd43d43f7ad630c903fd2a
Kit Fung <sa9510@gmail.com>
2020-12-01 14:48:25 +0800

    Done the cluster part

--- model_clustering.py
+++ model_clustering.py
@@ -2,25 +2,30 @@ import os
 import sys
 import yaml
 
+import numpy as np
 from sklearn.cluster import KMeans
 import torch
 from ptflops import get_model_complexity_info
+import pretrainedmodels
 
-input_shape = (3, 244, 244)
+input_shape = (3, 416, 416)
 
 # Do the clustering on torch model
 # # man craft feature: [sparity, number of fullcon, number of dropout, number of conv, number of activate, number of normalization, depth]
-# man craft feature: [flops, macs, memory, sparity]
-def gen_feature(model):
-    macs, params = get_model_complexity_info(net, input_shape, as_strings=True,
+# man craft feature: [macs, params, sparity]
+def gen_feature(model, sparisity):
+    macs, params = get_model_complexity_info(model, input_shape, as_strings=True,
                                            print_per_layer_stat=True, verbose=True)
-    return params
+    macs = float(macs.split()[0])
+    params = float(params.split()[0])
+    return (macs, params, sparisity)
+
 # https://github.com/Lyken17/pytorch-OpCounter
 # https://github.com/Swall0w/torchstat
 # https://github.com/sovrasov/flops-counter.pytorch
 
 def gen_features(models):
-    return {name: gen_feature(model) for name, model in models.items()}
+    return {name: gen_feature(model, sparisity) for name, (model, sparisity) in models.items()}
 
 # models: {
 #   model_a: feature vector,
@@ -29,22 +34,27 @@ def gen_features(models):
 #   model_d: feature vector,
 # }
 def clustering(features):
-    names = features.keys()
-    X = [features[k] for k in names]
-    kmeans = KMeans(n_clusters=2, random_state=0).fit(X)
-    kmeans.labels_
-    pass
+    names = list(features.keys())
+    X = np.array([list(features[k]) for k in names])
+    X_normed = X / X.max(axis=0)
+    kmeans = KMeans(n_clusters=2, random_state=0).fit(X_normed)
+    out = {names[i]: int(kmeans.labels_[i]) for i in range(len(names))}
+    return out
 
 # ## INPUT YAML
-# config
-#  - model_name_a: ./path/to/model_a
-#  - model_name_b: ./path/to/model_b
-#  - model_name_c: ./path/to/model_c
-#  - model_name_d: ./path/to/model_d
 def load_models(conf_file):
+    out = {}
     with open(conf_file) as f:
-        doc = yaml.full_load(file)
-        return doc['config']
+        doc = yaml.full_load(f)
+    models = doc['config']
+    for k, stat in models.items():
+        if stat['net'] == 'inception_v4':
+            out[k] = (pretrainedmodels.inceptionv4(pretrained=None), stat['sparisity'])
+        elif stat['net'] == 'resnet18':
+            out[k] = (pretrainedmodels.resnet18(pretrained=None), stat['sparisity'])
+        else:
+            raise "Unknown net"
+    return out
 
 # ## OUTPUT YAML
 # models_class:
@@ -62,10 +72,10 @@ def write_result(result):
         "cluster_center": {}
     }
     out["models_class"] = result
-    for k, v in result:
+    for k, v in result.items():
         out["cluster_center"][v] = k
-
-    with open("cluster.yaml", 'w') as f:
+    import pdb; pdb.set_trace()
+    with open("cluster_result.yaml", 'w') as f:
         yaml.dump(out, f)
 
 if __name__ == '__main__':
